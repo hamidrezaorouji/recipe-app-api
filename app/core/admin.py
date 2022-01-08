dae85b6dd25d2c4b6add9a5b8edf9b29b4a0c161
hamid <hamidrezaorouji74@gmail.com>
2022-01-08 22:09:18 +0330

    Configured postgres db

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
             _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'), {'fields':('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
